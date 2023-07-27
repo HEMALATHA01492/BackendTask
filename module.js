f71e7f502ecfd8ba83855b07151541eaac942f34
HEMALATHA01492 <hema01492@gmail.com>
2023-07-27 11:12:45 +0530

    first commit

---
+++ module.js
@@ -0,0 +1,13 @@
+// let a=new Date();
+// let year=(a.getFullYear());
+// let month=(a.getMonth()+1);
+// let date=(a.getDate());
+// let hour=(a.getHours());
+// let minutes=(a.getMinutes());
+// let seconds=(a.getSeconds());
+// let milliseconds=(a.getMilliseconds());
+// let fileName=`${year}${month}${date}-${hour}:${minutes}:${seconds}`
+// let content=`Current Time is: ${hour} hour: ${minutes} min: ${seconds} sec :${milliseconds} millisec`
+// console.log(a);
+// console.log(fileName);
+// console.log(content);
